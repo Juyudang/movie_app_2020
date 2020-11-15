5f66aa8ccacab714ba7c2914b562588820741c88
Seungmin Baek <yingking100@hanyang.ac.kr>
2020-11-15 13:49:03 +0900

    Updates

---
+++ precache-manifest.f8984c4e3a8a8660b5ffe933558d3c70.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4ee9541e45fcad6de2eddd08d2fd4e6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "822a4326fc7fd91e1e68",
+    "url": "/static/css/main.3dd3ea10.chunk.css"
+  },
+  {
+    "revision": "ca1290ec655463a20b0e",
+    "url": "/static/js/2.ad030fd9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.ad030fd9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "822a4326fc7fd91e1e68",
+    "url": "/static/js/main.c4f34592.chunk.js"
+  },
+  {
+    "revision": "bd7f4f55b55e38cea2f1",
+    "url": "/static/js/runtime-main.58002af7.js"
+  }
+]);
